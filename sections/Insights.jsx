385d3fb49080173f43bb0710a759c1bc3f6ede1d
Wozgard <wozgard@google.com>
2023-10-28 19:48:13 +0300

    feat: new title for Insight

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -16,8 +16,8 @@ const Insights = () => (
       viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
-      <TypingText title="| Some tips to play" textStyles="text-center" />
-      <TitleText title={<>Insight about metaverse</>} textStyles="text-center" />
+      <TypingText title="| insight about GG" textStyles="text-center" />
+      <TitleText title={<>Some tips to play</>} textStyles="text-center" />
       <div className="mt-[50px] flex flex-col gap-[30px]">
         {insights.map((item, index) => (
           <InsightCard key={`insight-${index}`} {...item} index={index + 1} />
